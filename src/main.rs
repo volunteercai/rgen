0f1b03b4827fcc4192bca5d3a81e47a4eb8dcacd
cai <caicai@gmail.com>
2022-08-11 23:04:51 +0800

    单元测试解析文件

--- src/main.rs
+++ src/main.rs
@@ -21,3 +21,30 @@ fn main() {
 	
 	// 生成代码
 }
+
+#[test]
+fn test_parse_reg() {
+	let reg = fs::read_to_string("entities.rge").unwrap();
+	print!("{}", reg);
+	// 解析.rge为entities
+	let entities = parse_reg(&reg);
+}
+
+fn parse_reg(f: &str) -> Vec<Entity> {
+	let mut entities = Vec::new();
+	let lines = f.lines();
+	for line in lines {
+		let line = line.trim();
+		if line.starts_with("entity") {
+			// 截取entity名称
+			let name = line.split(" ").nth(1).unwrap();
+			let mut entity = Entity {
+				name: name.to_string(),
+				low_case_name: name.to_string().to_lowercase(),
+				fileds: Vec::new(),
+			};
+			entities.push(entity);
+		}
+	}
+	entities
+}
